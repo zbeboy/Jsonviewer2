2efb3de85a70e63d95bfa4d74619b9ca2c93e8bb
zbeboy <863052317@qq.com>
2017-10-21 14:11:46 +0800

    Change to vs2017

---
+++ resource.h
@@ -0,0 +1,8 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDD_TREE                                101
+#define IDD_ABOUTDLG                            103
+#define IDC_TREE1                               1002
+#define IDC_WEB                                 1002
